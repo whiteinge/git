c24579aac9d2d01fd2ca33fd30ef73f69c194e3f
Abhishek Kumar <abhishekkumar8222@gmail.com>
2020-12-28 11:16:06 +0000

    commit-graph: use generation v2 only if entire chain does
    Since there are released versions of Git that understand generation
    numbers in the commit-graph's CDAT chunk but do not understand the GDAT
    chunk, the following scenario is possible:
    
    1. "New" Git writes a commit-graph with the GDAT chunk.
    2. "Old" Git writes a split commit-graph on top without a GDAT chunk.
    
    If each layer of split commit-graph is treated independently, as it was
    the case before this commit, with Git inspecting only the current layer
    for chunk_generation_data pointer, commits in the lower layer (one with
    GDAT) whould have corrected commit date as their generation number,
    while commits in the upper layer would have topological levels as their
    generation. Corrected commit dates usually have much larger values than
    topological levels. This means that if we take two commits, one from the
    upper layer, and one reachable from it in the lower layer, then the
    expectation that the generation of a parent is smaller than the
    generation of a child would be violated.
    
    It is difficult to expose this issue in a test. Since we _start_ with
    artificially low generation numbers, any commit walk that prioritizes
    generation numbers will walk all of the commits with high generation
    number before walking the commits with low generation number. In all the
    cases I tried, the commit-graph layers themselves "protect" any
    incorrect behavior since none of the commits in the lower layer can
    reach the commits in the upper layer.
    
    This issue would manifest itself as a performance problem in this case,
    especially with something like "git log --graph" since the low
    generation numbers would cause the in-degree queue to walk all of the
    commits in the lower layer before allowing the topo-order queue to write
    anything to output (depending on the size of the upper layer).
    
    Therefore, When writing the new layer in split commit-graph, we write a
    GDAT chunk only if the topmost layer has a GDAT chunk. This guarantees
    that if a layer has GDAT chunk, all lower layers must have a GDAT chunk
    as well.
    
    Rewriting layers follows similar approach: if the topmost layer below
    the set of layers being rewritten (in the split commit-graph chain)
    exists, and it does not contain GDAT chunk, then the result of rewrite
    does not have GDAT chunks either.
    
    Signed-off-by: Derrick Stolee <dstolee@microsoft.com>
    Signed-off-by: Abhishek Kumar <abhishekkumar8222@gmail.com>

--- commit-graph.c
+++ commit-graph.c
@@ -610,6 +610,21 @@ static struct commit_graph *load_commit_graph_chain(struct repository *r,
 	return graph_chain;
 }
 
+static void validate_mixed_generation_chain(struct commit_graph *g)
+{
+	int read_generation_data;
+
+	if (!g)
+		return;
+
+	read_generation_data = !!g->chunk_generation_data;
+
+	while (g) {
+		g->read_generation_data = read_generation_data;
+		g = g->base_graph;
+	}
+}
+
 struct commit_graph *read_commit_graph_one(struct repository *r,
 					   struct object_directory *odb)
 {
@@ -617,6 +632,8 @@ struct commit_graph *read_commit_graph_one(struct repository *r,
 
 	if (!g)
 		g = load_commit_graph_chain(r, odb);
+
+	validate_mixed_generation_chain(g);
 
 	return g;
 }
@@ -787,7 +804,7 @@ static void fill_commit_graph_info(struct commit *item, struct commit_graph *g, 
 	date_low = get_be32(commit_data + g->hash_len + 12);
 	item->date = (timestamp_t)((date_high << 32) | date_low);
 
-	if (g->chunk_generation_data) {
+	if (g->read_generation_data) {
 		offset = (timestamp_t)get_be32(g->chunk_generation_data + sizeof(uint32_t) * lex_index);
 
 		if (offset & CORRECTED_COMMIT_DATE_OFFSET_OVERFLOW) {
@@ -2012,6 +2029,13 @@ static void split_graph_merge_strategy(struct write_commit_graph_context *ctx)
 		if (i < ctx->num_commit_graphs_after)
 			ctx->commit_graph_hash_after[i] = xstrdup(oid_to_hex(&g->oid));
 
+		/*
+		 * If the topmost remaining layer has generation data chunk, the
+		 * resultant layer also has generation data chunk.
+		 */
+		if (i == ctx->num_commit_graphs_after - 2)
+			ctx->write_generation_data = !!g->chunk_generation_data;
+
 		i--;
 		g = g->base_graph;
 	}
@@ -2239,6 +2263,7 @@ int write_commit_graph(struct object_directory *odb,
 		struct commit_graph *g = ctx->r->objects->commit_graph;
 
 		while (g) {
+			g->read_generation_data = 1;
 			g->topo_levels = &topo_levels;
 			g = g->base_graph;
 		}
@@ -2534,7 +2559,7 @@ int verify_commit_graph(struct repository *r, struct commit_graph *g, int flags)
 		 * also GENERATION_NUMBER_V1_MAX. Decrement to avoid extra logic
 		 * in the following condition.
 		 */
-		if (!g->chunk_generation_data && max_generation == GENERATION_NUMBER_V1_MAX)
+		if (!g->read_generation_data && max_generation == GENERATION_NUMBER_V1_MAX)
 			max_generation--;
 
 		generation = commit_graph_generation(graph_commit);
